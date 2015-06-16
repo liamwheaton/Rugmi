6e3f9786e80d0a4de08e4de2faa55402f565ecec
Liam Wheaton <liam.wheaton@44390-501.auckland.natcoll.ac.nz>
2015-06-16 16:44:40 +1200

    added upload test images

---
+++ public/user.php
@@ -0,0 +1,23 @@
+<?php 
+
+	#libraries
+
+require_once '../libraries/login.lib.php';
+require_once '../libraries/url.lib.php';
+require_once '../libraries/form.lib.php';
+
+require_once '../models/image.model.php';
+require_once '../models/images.collection.php';
+require_once '../models/user.model.php';
+
+	#logic
+
+
+
+	#views/redirect
+
+include 'header.php';
+include 'nav.php';
+include 'user_profile.php';
+include 'footer.php';
+
---
+++ views/user_profile.php
@@ -0,0 +1,39 @@
+<div class="container">
+
+	<?php $id = $_GET['id']; ?>
+
+	<?php $user = new User(); ?>
+
+	<?php $user->load($id); ?>
+	
+	<h1>User profile</h1>
+
+	<?php $images = New Images_Collection([
+			'deleted' => '0'
+		]); 
+	?>
+	
+	<div class="flex flex-wrap space-between">
+
+		<?php foreach ($images->items as $user_image): ?>
+
+			<div class="image_container flex flex-column">
+
+				<img class ="image" src="/assets/uploads/<?= $user_image->url ?>" alt="">
+				
+				<div class="caption"><p><?= $user_image->caption ?></p></div>
+				
+				<div class="controls">
+
+					<a href="delete_image.php?id=<?= $user_image->id ?>" class="delete">Delete</a>
+					<a href="edit_image.php?id=<?= $user_image->id ?>" class="edit">Edit</a>
+					
+				</div>
+
+			</div>
+			
+		<?php endforeach ?>
+
+	</div>
+
+</div>
