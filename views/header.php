9ce95994dac70ae412715eb0461844f8aa8e960d
Thomas Caiger <thomas.caiger@44397-501.auckland.natcoll.ac.nz>
2015-06-16 15:46:56 +1200

    Fixed Css file

--- views/header.php
+++ views/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>RUGMI</title>
-	<link rel="stylesheet" href="../assets/css/style.css">
+	<link rel="stylesheet" href="../public/assets/css/style.css">
 </head>
 <body>
 	
---
+++ views/nav.php
@@ -0,0 +1,10 @@
+<nav class="flex">
+	<h1>RUGMI</h1>
+	<ul>
+		<li>Upload Image</li>
+	</ul>
+	<ul class="flex">
+		<li>Sign In</li>
+		<li>Sign Up</li>
+	</ul>
+</nav>
