24624b35e9b7dde7b449e09296916ce59c1f4f86
Liam Wheaton <liam.wheaton@44390-501.auckland.natcoll.ac.nz>
2015-06-18 18:14:44 +1200

    added updated css to image_list

--- views/image_list.php
+++ views/image_list.php
@@ -4,10 +4,21 @@
 	<?php $images = New Images_Collection(['deleted' => '0']); ?>
 	<?php foreach ($images->items as $user_image): ?>
 
-		<div class="image-container">
+		<div class="card flex flex-column">
+			
+			<div class="image-container">
 
-			<a href="single_view.php?image_id=<?= $user_image->id ?>"><img class ="image" src="assets/uploads/<?= $user_image->url ?>" alt=""></a>
+				<a href="single_view.php?image_id=<?= $user_image->id ?>"><img class ="image" src="assets/uploads/<?= $user_image->url ?>" alt=""></a>
 
+			</div>
+
+			<p class = 'date'>posted <?= $user_image->date_posted ?> hours ago</p>
+
+			<div class="controls flex flex-j-between">
+				
+				<a href="" class="flat-button-grey">3 comments</a>
+				
+			</div>
 		</div>
 			
 	<?php endforeach ?>
